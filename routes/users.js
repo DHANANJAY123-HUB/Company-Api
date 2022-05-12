6fca38dfd12894e8f2651e67788d583721826ed9
DHANANJAY123-HUB <75534106+DHANANJAY123-HUB@users.noreply.github.com>
2022-05-12 18:59:47 +0530

    commit

--- routes/users.js
+++ routes/users.js
@@ -190,5 +190,23 @@ router.delete('/:id', async (req, res) => {
     }
 })
 
+//Update by ID Method
+router.patch('/:id', async (req, res) => {
+    try {
+        const id = req.params.id;
+        const updatedData = req.body;
+        const options = { new: true };
+
+        const result = await indexModel.findByIdAndUpdate(
+            id, updatedData, options
+        )
+
+        res.send(result)
+    }
+    catch (error) {
+        res.status(400).json({ message: error.message })
+    }
+})
+
 
 module.exports = router;
