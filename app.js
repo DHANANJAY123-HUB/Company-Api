fa650c9d5c179e208929bd98e3ea19c37aff7d34
DHANANJAY123-HUB <75534106+DHANANJAY123-HUB@users.noreply.github.com>
2022-05-12 10:27:41 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var session = require('express-session');
 const indexModel = require('./model/indexModel')
-var imgModel = require('./model/imageModel');
+//var imgModel = require('./model/imageModel');
 
 
 var indexRouter = require('./routes/index');
